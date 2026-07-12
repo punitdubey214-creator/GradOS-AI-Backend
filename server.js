d5907adf9068055fbc4c7b86971969517ddc0d83
Punit Dubey <punitdubey214@gmail.com>
2026-07-12 23:40:44 +0530

    Start Express server

--- server.js
+++ server.js
@@ -236,3 +236,8 @@ app.post("/share/create", async (req, res) => {
   }
 
 });
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
